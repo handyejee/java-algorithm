d0a322c0c20838128bc4b01ad5644c13814c54bc
yjson <handyejee14@gmail.com>
2024-05-20 22:19:36 +0900

    백준 ATM 그리디 알고리즘 문제

---
+++ src/greedy/B11399.java
@@ -0,0 +1,33 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class B11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 사람 수
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // StringTokenizer는 초기화 시점에 지정된 입력 줄을 기준으로 토큰을 나누기 때문에, 적절한 데이터가 전달되도록 초기화 순서를 맞춰야 힌다.
+
+        int[] pArr = new int[N];
+        int sum = 0;
+        int total = 0;
+
+        for (int i = 0; i < N; i++) {
+            pArr[i] = Integer.parseInt(st.nextToken()); // 각 사람이 인출하는데 걸리는 시간
+        }
+        Arrays.sort(pArr);
+
+        for (int i = 0; i < N; i++) {
+            sum += pArr[i];
+            total += sum;
+        }
+        System.out.println(total);
+    }
+}
