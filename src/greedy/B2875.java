8a3f0bae6871a96c8cc31f1959dad39429a6d674
yjson <handyejee14@gmail.com>
2024-05-24 23:52:24 +0900

    백준 대회, 인턴 그리디 문제

--- src/greedy/B2875.java
+++ src/greedy/B2875.java
@@ -4,21 +4,25 @@ import java.io.*;
 import java.util.*;
 
 public class B2875 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int team = 0;
-        while (N >=2 && M >= 1 && (N + M) - K >= 3) {
 
-            N -=2;
-            M -=1;
-            team++;
+        int n = Integer.parseInt(st.nextToken()); // 여학생
+        int m = Integer.parseInt(st.nextToken()); // 남학생
+        int k = Integer.parseInt(st.nextToken()); // 인턴참여
+
+        int count = 0;
+        while(true){
+            n -= 2;
+            m -= 1;
+
+            if (n < 0 || m < 0 || n + m < k){
+                break;
+            }
+            count += 1;
         }
-        System.out.println(team);
+        System.out.println(count);
     }
 
 }
