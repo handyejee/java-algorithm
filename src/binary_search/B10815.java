364d39f4eb63f89fac6cf6feb22a7ae703ce9f3e
yjson <handyejee14@gmail.com>
2024-06-14 01:58:21 +0900

    백준 이진탐색 숫자찾기 문제 메서드 분리해서 다시 구현

--- src/binary_search/B10815.java
+++ src/binary_search/B10815.java
@@ -1,54 +1,55 @@
 package binary_search;
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class B10815 {
+class B1085 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine()); // 자연수 N
-        int[] numArr = new int[N]; // N 담을 배열 선언
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            numArr[i] = Integer.parseInt(st.nextToken());
-        }
+            int cardNum = Integer.parseInt(br.readLine()); // 숫자카드 개수
+            int[] cardArr = new int[cardNum]; // 숫자카드 담을 배열
 
-        Arrays.sort(numArr); // 오름차순 정렬
-
-        int M = Integer.parseInt(br.readLine()); // 숫자 M
-        StringBuilder sb = new StringBuilder(); // 결과 출력 위한 StringBuilder 선언
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int findNum = Integer.parseInt(st.nextToken()); // M의 크기만큼 찾을 숫자 입력받음
-            boolean found = false;
-
-            int low = 0;
-            int high = numArr.length -1;
-
-            while (low <= high){
-                int mid = (low + high) / 2;
-
-                if (findNum < numArr[mid]) {
-                    high = mid - 1;
-
-                }else if(findNum > numArr[mid]){
-                    low = mid + 1;
-
-                } else {
-                    found = true;
-                    break;
-                }
-            }
-            if(found) {
-                sb.append(1).append("\n");
-            }
-            else {
-                sb.append(0).append("\n");
+            StringTokenizer st = new StringTokenizer(br.readLine(), " "); // String 받아오기
+            for(int i = 0; i < cardNum; i++) {
+                cardArr[i] = Integer.parseInt(st.nextToken());
             }
 
-        }
+            Arrays.sort(cardArr);
+
+            int M = Integer.parseInt(br.readLine()); // 찾을 카드의 개수
+            st = new StringTokenizer(br.readLine(), " ");
+
+            StringBuilder sb = new StringBuilder(); // 결과값 담아줄 StringBuilder 선언
+
+            for (int i = 0; i < M; i++) {
+                int findCard = Integer.parseInt(st.nextToken());
+                sb.append(binarySearch(cardArr, findCard)).append(" ");
+            }
         System.out.println(sb);
+    }
+
+    public static int binarySearch(int[] cardArr, int findCard) { // 이진탐색 메서드
+        int min = 0;
+        int max = cardArr.length - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            // 찾는 값이 중간보다 클때
+            if (findCard > cardArr[mid] ) {
+                min = mid + 1;
+
+            // 찾는 값이 중간 보다 작을때
+            } else if (findCard < cardArr[mid]) {
+                max = mid - 1;
+
+             // 찾는 값과 일치하는 경우
+            } else {
+                return 1;
+            }
+        }
+        // 찾는 값이 없는 경우
+        return 0;
     }
 }
