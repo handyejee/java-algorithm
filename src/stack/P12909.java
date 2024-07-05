015eeb12a51133c27fc1d1d7f61e36ac392e7996
yjson <handyejee14@gmail.com>
2024-07-05 23:56:11 +0900

    프로그래머스 스택 올바른 괄호 문제 풀이

---
+++ src/stack/P12909.java
@@ -0,0 +1,23 @@
+package stack;
+
+import java.util.Stack;
+
+class P12909 {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()){
+            if(c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) { // 닫힌 괄호만 들어온 경우
+                    return false;
+                }
+                else { // 짝이 맞는 경우
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
