20fc768aed703f02bf16c896253be7811073e1ac
yjson <handyejee14@gmail.com>
2025-06-06 13:47:58 +0900

    백준 세로읽기 풀이

---
+++ src/bakjoon/b10798/B10798.java
@@ -0,0 +1,26 @@
+package bakjoon.b10798;
+
+import java.io.*;
+import java.util.*;
+
+public class B10798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[][] arr = new char[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < line.length(); j++) {
+              arr[i][j]  = line.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[i][j] != '\0') {
+                    System.out.print(arr[i][j]);
+                }
+            }
+        }
+    }
+}
